9ccf91b3315c261a30e918ee3be84a470b963201
KoNEW <konev.vn@gmail.com>
2017-07-12 18:20:01 +0300

    Vertical table patterns

---
+++ lamb/db/patterns.py
@@ -0,0 +1,220 @@
+# -*- coding: utf-8 -*-
+__author__ = 'KoNEW'
+
+import enum
+from sqlalchemy.dialects.mysql import INTEGER, VARCHAR
+from sqlalchemy import Column, Enum
+from sqlalchemy_utils import get_primary_keys
+from lamb.db.session import DeclarativeBase
+from lamb.db.context import lamb_db_context
+from lamb.rest.exceptions import ServerError
+
+__all__ = [
+    'DbEnum', 'ConfigEnum'
+]
+
+def get_class_by_name(base, classname):
+    """
+    :param base: Declarative model base
+    :param table: SQLAlchemy Table name
+    :return: Declarative class or None.
+    """
+    return base._decl_class_registry[classname]
+
+@enum.unique
+class DbEnum(enum.Enum):
+    """Abstract enum to database processor.
+
+    Abstract class that maps enum class to database table for store values.
+
+    Example:
+
+        import enum
+        from lamb.db.patterns import DbEnum
+
+        @enum.unique
+        class TestPlain(DbEnum):
+            value1 = 'value1'
+            value2 = 'value2'
+            value3 = 'value3'
+
+            __table_class__ = '_TestPlainTable'
+
+        class _TestPlainTable(DeclarativeBase):
+            __tablename__ = 'test_plain'
+            pk_code = Column(
+                Enum(TestPlain),
+                nullable=False,
+                primary_key=True
+            )
+
+        TestPlain.value1.db_flush()
+        print(TestPlain.value1)
+    """
+
+    __table_class__ = None
+    __attrib_mapping__ = {}
+
+    def __new__(cls, code, *args, **kwargs):
+        obj = object.__new__(cls)
+        obj._value_ = code
+        return obj
+
+    def _table_class(self):
+        """
+        :return: Database ORM table class
+        :rtype: DeclarativeBase
+        """
+        if isinstance(self.__class__.__table_class__, str):
+            return get_class_by_name(DeclarativeBase, self.__class__.__table_class__)
+        elif isinstance(self.__class__.__table_class__, DeclarativeBase):
+            return self.__class__.__table_class__
+        else:
+            raise ServerError(
+                'Improperly configured class %s. Could not locate table class name' % self.__class__.__name__)
+
+    def _setup_db_item(self, item):
+        """ Designed item initializer
+
+        This method should be overriden in subclass if they have custom fields on database level.
+        By default method only assign primary key of underline table to value of self instance.
+
+        :param item: Database table record instance
+        :type item: lamb.db.session.DeclarativeBase
+        :return: Initialized table record instance
+        :rtype: lamb.db.session.DeclarativeBase
+        """
+        pk = get_primary_keys(item)
+        pk_column_name, pk_column_description = pk.popitem()
+        setattr(item, pk_column_name, self.value)
+        return item
+
+    def _db_item(self, session):
+        """
+        :param session: Databse session instance
+        :type session: sqlalchemy.orm.Session
+        :return: Initialized/extracted table record instance
+        :rtype: lamb.db.session.DeclarativeBase
+        """
+        table_class = self._table_class()
+        result = session.query(table_class).get(self.value)
+        if result is None:
+            result = table_class()
+            self._setup_db_item(item=result)
+            session.add(result)
+            session.commit()
+        return result
+
+    @classmethod
+    def db_map(cls, db_session):
+        return {c:c._db_item(db_session) for c in cls}
+
+    def db_flush(self):
+        """
+        :return: Explicitily flush enum value to database by touching it within session
+        :rtype: None
+        """
+        with lamb_db_context() as session:
+            _ = self._db_item(session)
+
+    def __getattribute__(self, key):
+        if key[:2] != '__':
+            mapping = self.__class__.__attrib_mapping__
+            if key in mapping:
+                mapped_key = mapping[key]
+                with lamb_db_context() as session:
+                    db_item = self._db_item(session)
+                    return getattr(db_item, mapped_key)
+        return super().__getattribute__(key)
+
+    def __setattr__(self, key, value):
+        if key[:2] != '__':
+            mapping = self.__class__.__attrib_mapping__
+            if key in mapping:
+                mapped_key = mapping[key]
+                with lamb_db_context() as session:
+                    db_item = self._db_item(session)
+                    db_item.__setattr__(mapped_key, value)
+                    session.commit()
+        super().__setattr__(key, value)
+
+    def __dir__(self):
+        if isinstance(self.__class__.__attrib_mapping__, dict):
+            return super().__dir__() + [str(k) for k in self.__class__.__attrib_mapping__.keys()]
+        else:
+            return super().__dir__()
+
+@enum.unique
+class ConfigEnum(DbEnum):
+
+    """Config storage processor.
+
+    Class that support store of values of enum in database.
+    Support defaults/headers for each item that not stored in database
+
+    Example:
+
+        import enum
+        from lamb.db.patterns import ConfigEnum
+
+        @enum.unique
+        class TestCode(ConfigEnum):
+            Code1 = ('code1', 1, 'Header1')
+            Code2 = ('code2', 2, 'Header2')
+            Code3 = ('code3', 3, 'Header3')
+            Code4 = ('code4', 4, 'Header4')
+            Code5 = ('code5', 5, 'Header5')
+
+            __table_class__ = '_TestCodeTable'
+            __attrib_mapping__ = {'val':'value'}
+
+
+        class _TestCodeTable(DeclarativeBase):
+            __tablename__ = 'test_codes'
+            code = Column(
+                Enum(TestCode),
+                nullable=False,
+                primary_key=True,
+                default=TestCode.Code1,
+                server_default=TestCode.Code1.value
+            )
+            value = Column(INTEGER(unsigned=True), nullable=False)
+
+
+        @enum.unique
+        class TestSettings(ConfigEnum):
+            settings1 = ('settings1',  1, 'ololo')
+            settings2 = ('settings2',  2, 'ololo')
+            settings3 = ('settings3',  3, 'ololo')
+
+            __table_class__ = '_TestSettingsTable'
+            __attrib_mapping__ = {'val':'value'}
+
+
+        class _TestSettingsTable(DeclarativeBase):
+            __tablename__ = 'test_settings'
+            variable = Column(
+                VARCHAR(50),
+                nullable=False,
+                primary_key=True
+            )
+            value = Column(
+                VARCHAR(100),
+                nullable=True
+            )
+
+        TestCode.Code1.val = 100
+        TestSettings.settings1.val = 10
+    """
+
+    def __new__(cls, code, default, header, *args, **kwargs):
+        obj = object.__new__(cls)
+        obj._value_ = code
+        obj.default = default
+        obj.header = header
+        return obj
+
+    def _setup_db_item(self, item):
+        item = super()._setup_db_item(item=item)
+        item.value = self.default
+        return item
