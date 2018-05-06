6420e3343e8a60df1fa24ce7a4ee3de88e762794
Vladimir Konev <konev.vn@gmail.com>
2018-05-06 13:41:59 +0300

    locale type

---
+++ lamb/types/locale.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+__author__ = 'KoNEW'
+
+import logging
+import babel
+
+from sqlalchemy import types
+from sqlalchemy_utils.types.scalar_coercible import ScalarCoercible
+
+from ..rest.exceptions import ServerError, InvalidParamValueError
+from ..json.mixins import ResponseEncodableMixin
+
+__all__ = [
+    'LambLocale', 'LambLocaleType'
+]
+
+logger = logging.getLogger(__name__)
+
+
+class LambLocale(ResponseEncodableMixin, babel.Locale):
+    def response_encode(self, request=None):
+        return str(self)
+
+
+class LambLocaleType(types.TypeDecorator, ScalarCoercible):
+    """
+    LambLocaleType based on sqlalchemy_utils LocaleType data field.
+    """
+
+    impl = types.Unicode(10)
+    python_type = LambLocale
+
+    def process_bind_param(self, value, dialect):
+        if isinstance(value, LambLocale):
+            return str(value)
+
+        if isinstance(value, str):
+            return value
+
+    def process_result_value(self, value, dialect):
+        if value is not None:
+            try:
+                return LambLocale.parse(value)
+            except (ValueError, babel.UnknownLocaleError) as e:
+                raise ServerError('Locale fields configured invalid') from e
+
+    def _coerce(self, value):
+        if value is not None and not isinstance(value, LambLocale):
+            try:
+                return LambLocale.parse(value)
+            except (ValueError, babel.UnknownLocaleError) as e:
+                raise InvalidParamValueError('Unknown or unsupported locale value %s' % value)
+        return value
