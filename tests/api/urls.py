bdc5292abe46b695042aa1b0181b27b60c3dafca
k.golubev <ff.kirill@gmail.com>
2018-12-17 14:29:51 +0300

    Added handlers to use as standard Django's 400, 404, 500 views

--- tests/api/urls.py
+++ tests/api/urls.py
@@ -1,4 +1,9 @@
-from django.conf.urls import url
-
 urlpatterns = [
 ]
+
+
+handler404 = 'lamb.utils.default_views.page_not_found'
+
+handler400 = 'lamb.utils.default_views.bad_request'
+
+handler500 = 'lamb.utils.default_views.server_error'
--- tests/middleware/urls.py
+++ tests/middleware/urls.py
@@ -4,3 +4,9 @@ from django.conf.urls import include
 urlpatterns = [
     url(r'^', include('tests.middleware.test_urls', namespace='tests')),
 ]
+
+handler404 = 'lamb.utils.default_views.page_not_found'
+
+handler400 = 'lamb.utils.default_views.bad_request'
+
+handler500 = 'lamb.utils.default_views.server_error'
