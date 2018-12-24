01178f695b4e721c4951bcc04d4e67eaef79484c
v.konev <konev.vn@gmail.com>
2018-12-24 15:35:26 +0300

    refactoring

--- lamb/__init__.py
+++ lamb/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 __author__ = 'KoNEW'
 
-__version__ = '1.0.36'
+__version__ = '2.0.0'
 
 
 import logging
--- setup.py
+++ setup.py
@@ -21,9 +21,7 @@ except ImportError:
 
 
 def get_version(package):
-    """
-    Return package version as listed in `__version__` in `init.py`.
-    """
+    """ Return package version as listed in `__version__` in `init.py`. """
     init_py = open(os.path.join(package, '__init__.py')).read()
     return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
 
@@ -32,19 +30,14 @@ version = get_version('lamb')
 
 
 def get_packages(package):
-    """
-    Return root package and all sub-packages.
-    """
+    """ Return root package and all sub-packages. """
     return [dirpath
             for dirpath, dirnames, filenames in os.walk(package)
             if os.path.exists(os.path.join(dirpath, '__init__.py'))]
 
 
 def get_package_data(package):
-    """
-    Return all files under the root package, that are not in a
-    package themselves.
-    """
+    """ Return all files under the root package, that are not in a package themselves. """
     walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
             for dirpath, dirnames, filenames in os.walk(package)
             if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
