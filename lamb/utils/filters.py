7ab2fccb00d19e1a92822a8bd593392e1b0b8e47
Владимир Конев <konev.vn@gmail.com>
2019-07-11 16:31:01 +0300

    chnages

--- lamb/utils/filters.py
+++ lamb/utils/filters.py
@@ -31,6 +31,7 @@ logger = logging.getLogger(__name__)
 # abstract
 T = TypeVar('T')
 
+
 # TODO: migrate to dataclasses
 class Filter(object):
     """ Abstract filter for model query """
@@ -231,6 +232,7 @@ class ColumnBooleanFilter(ColumnValueFilter):
 
         super().__init__(*args, **kwargs)
         # kwargs['req_type']
+
 
 class EnumFilter(ColumnValueFilter):
 
