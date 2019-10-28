ae8c77378a569840dd97c148dd022a958c351890
Владимир Конев <konev.vn@gmail.com>
2019-10-28 14:50:59 +0300

    database settings on no pooled scheme

--- lamb/db/context.py
+++ lamb/db/context.py
@@ -5,6 +5,7 @@ import logging
 import sqlalchemy as sa
 
 from .session import lamb_db_session_maker
+from sqlalchemy.orm.session import Session  as SASession
 
 
 logger = logging.getLogger(__name__)
@@ -15,9 +16,15 @@ __all__ = ['lamb_db_context']
 
 class lamb_db_context:
 
-    def __enter__(self) -> sa.orm.session.Session:
+    _pooled: bool
+
+    def __init__(self, pooled: bool = True):
+        super().__init__()
+        self._pooled = pooled
+
+    def __enter__(self) -> SASession:
         logger.debug('Enter lamb database context')
-        self.db_session = lamb_db_session_maker()
+        self.db_session = lamb_db_session_maker(pooled=self._pooled)
         return self.db_session
 
     def __exit__(self, exc_type, exc_val, exc_tb):
