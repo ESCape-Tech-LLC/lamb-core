2ec4ab4305f3754293c4ddf0858d0290b7a799b1
Anton Lapshin <anton.lapshin.b@gmail.com>
2020-09-07 18:47:44 +0300

    Implement event_logging models

---
+++ lamb/service/event_logging/models.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+
+import enum
+from datetime import datetime
+from typing import List
+
+from sqlalchemy import Column, ForeignKey, BIGINT, TIMESTAMP, VARCHAR, text
+from sqlalchemy.orm import relationship, validates
+from sqlalchemy_utils import UUIDType
+
+from lamb.db.session import DeclarativeBase
+from lamb.json.mixins import ResponseEncodableMixin
+from lamb.types import DeviceInfoType
+from lamb.types.intenum import IntEnumType
+from lamb.types.jsonb import SUAJSONBType
+
+
+IP_MAX_LENGTH = 20
+
+
+class EventSourceType(enum.IntEnum):
+    SERVER = 0
+    CLIENT = 1
+
+
+class EventTrack(ResponseEncodableMixin, DeclarativeBase):
+    __tablename__ = 'lamb_event_track'
+
+    # columns
+    track_id = Column(UUIDType(binary=True, native=True), nullable=False, primary_key=True)
+    source = Column(
+        IntEnumType(EventSourceType),
+        nullable=False,
+        default=EventSourceType.SERVER,
+        server_default=str(EventSourceType.SERVER.value)
+    )
+    device_info = Column(DeviceInfoType, nullable=False, default=None, server_default=text('NULL'))
+    ip_address = Column(VARCHAR(IP_MAX_LENGTH), nullable=True, default=None, server_default=text('NULL'))
+
+    # relations
+    records = relationship('EventRecord', cascade='all', uselist=True)  # type: List[EventRecord]
+
+
+class EventRecord(ResponseEncodableMixin, DeclarativeBase):
+    __tablename__ = 'lamb_event_record'
+
+    # columns
+    record_id = Column(BIGINT, nullable=False, primary_key=True, autoincrement=True)
+    track_id = Column(
+        UUIDType(binary=True, native=True),
+        ForeignKey(EventTrack.track_id, onupdate='CASCADE', ondelete='CASCADE'),
+        nullable=False
+    )
+    time_mark = Column(TIMESTAMP, nullable=False, default=datetime.now, server_default=text('CURRENT_TIMESTAMP'))
+    context = Column(SUAJSONBType(native=True), nullable=True)
+
+    # relations
+    track = relationship(EventTrack, uselist=False)  # type: EventTrack
+
+    @validates('ip_address')
+    def validate_variable_name(self, key, value):
+        if not isinstance(value, str):
+            return value
+
+        return value[:IP_MAX_LENGTH]
