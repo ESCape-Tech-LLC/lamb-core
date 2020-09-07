ef2029141ba53fa90f5b2c1e23a4620601cfd573
Anton Lapshin <anton.lapshin.b@gmail.com>
2020-09-07 23:39:39 +0300

    Implement custom event track and event record models from lamb origins

--- lamb/service/event_logging/models.py
+++ lamb/service/event_logging/models.py
@@ -4,7 +4,8 @@ import enum
 from datetime import datetime
 from typing import List
 
-from sqlalchemy import Column, ForeignKey, BIGINT, BOOLEAN, TIMESTAMP, VARCHAR, text
+from sqlalchemy import Column, ForeignKey, BOOLEAN, TIMESTAMP, VARCHAR, text
+from sqlalchemy.ext.declarative import declared_attr
 from sqlalchemy.orm import relationship, validates
 from sqlalchemy_utils import UUIDType
 
@@ -27,40 +28,50 @@ class EventSourceType(enum.IntEnum):
 
 
 class EventTrack(ResponseEncodableMixin, DeclarativeBase):
+    __abstract__ = True
+
     __tablename__ = 'lamb_event_track'
 
     # columns
-    track_id = Column(UUIDType(binary=True, native=True), nullable=False, primary_key=True)
+    track_id = Column(UUIDType(binary=True, native=True), nullable=False, primary_key=True,
+                      server_default=text('gen_random_uuid()'))
     source = Column(
         IntEnumType(EventSourceType),
         nullable=False,
         default=EventSourceType.SERVER,
         server_default=str(EventSourceType.SERVER.value)
     )
-    device_info = Column(DeviceInfoType, nullable=False, default=None, server_default=text('NULL'))
+    device_info = Column(DeviceInfoType, nullable=True, default=None, server_default=text('NULL'))
     ip_address = Column(VARCHAR(IP_MAX_LENGTH), nullable=True, default=None, server_default=text('NULL'))
-
-    # relations
-    records = relationship('EventRecord', cascade='all', uselist=True)  # type: List[EventRecord]
 
 
 class EventRecord(ResponseEncodableMixin, DeclarativeBase):
+    __abstract__ = True
+
     __tablename__ = 'lamb_event_record'
+
+    __event_track_model__ = EventTrack
 
     # columns
     record_id = Column(UUIDType(binary=True, native=True), nullable=False, primary_key=True,
                        server_default=text('gen_random_uuid()'))
-    track_id = Column(
-        UUIDType(binary=True, native=True),
-        ForeignKey(EventTrack.track_id, onupdate='CASCADE', ondelete='CASCADE'),
-        nullable=False
-    )
+
+    @declared_attr
+    def track_id(self):
+        return Column(
+            UUIDType(binary=True, native=True),
+            ForeignKey(f'{self.__event_track_model__.__tablename__}.track_id', onupdate='CASCADE', ondelete='CASCADE'),
+            nullable=False
+        )
+
     timemark = Column(TIMESTAMP, nullable=False, default=datetime.now, server_default=text('CURRENT_TIMESTAMP'))
     timemark_ntp = Column(BOOLEAN, nullable=False, default=False, server_default=text('FALSE'))
     context = Column(SUAJSONBType(native=True), nullable=True)
 
     # relations
-    track = relationship(EventTrack, uselist=False)  # type: EventTrack
+    @declared_attr
+    def track(self):
+        return relationship(self.__event_track_model__.__name__, uselist=False, backref='records')  # type: EventTrack
 
     @validates('ip_address')
     def validate_variable_name(self, key, value):
