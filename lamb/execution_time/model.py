3df9598b8ab1fd99facb296e6a1a1c3ed2db9b02
Anton Lapshin <anton.lapshin.b@gmail.com>
2023-08-14 20:12:50 +0300

    Implement JSONType

--- lamb/execution_time/model.py
+++ lamb/execution_time/model.py
@@ -21,17 +21,14 @@ from sqlalchemy import (
 )
 from sqlalchemy.orm import relationship
 from sqlalchemy.engine import Connection
-from sqlalchemy.dialects.postgresql import JSONB
 
 # Lamb Framework
 from lamb.exc import ImproperlyConfiguredError
+from lamb.types import JSONType, DeviceInfoType
 from lamb.db.session import DeclarativeBase
 from lamb.json.mixins import ResponseEncodableMixin
 
 __all__ = ["LambExecutionTimeMarker", "LambExecutionTimeMetric"]
-
-# Lamb Framework
-from lamb.types import DeviceInfoType
 
 logger = logging.getLogger(__name__)
 
@@ -47,13 +44,12 @@ class LambExecutionTimeMetric(ResponseEncodableMixin, DeclarativeBase):
     app_name = Column(VARCHAR(100))
     url_name = Column(VARCHAR(100))
     http_method = Column(VARCHAR(15))
-    # TODO: Migrate headers, args, and context to custom JSONB type
-    headers = Column(JSONB)
-    args = Column(JSONB)
+    headers = Column(JSONType)
+    args = Column(JSONType)
     device_info = Column(DeviceInfoType, nullable=True, default=None, server_default=text("NULL"))
     status_code = Column(SMALLINT)
     elapsed_time = Column(FLOAT(), nullable=False, default=0.0, server_default=text("0"))
-    context = Column(JSONB)
+    context = Column(JSONType)
 
     # relations
     markers = relationship(
