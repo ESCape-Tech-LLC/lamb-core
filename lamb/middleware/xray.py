f458b4a48ed5895c5294663ffac4a0922cf0cbd9
gukov <a.gukov@escape-team.tech>
2024-05-29 11:35:55 +0300

    fix: LambXRayMiddleware xray header extraction

--- lamb/middleware/xray.py
+++ lamb/middleware/xray.py
@@ -20,25 +20,33 @@ logger = logging.getLogger(__name__)
 class LambXRayMiddleware(AsyncMiddlewareMixin):
     """Simple middleware that will generate and attach to request trace_id - formatted uuid string"""
 
-    @lazy
-    def _xray_header(cls):
-        logger.info(f"<{cls.__class__.__name__}>. xray header requested")
-        try:
-            result = get_settings_value(
-                "LAMB_LOGGING_HEADER_XRAY", "LAMB_EVENT_LOGGING_HEADER_TRACKID", req_type=str, allow_none=False
-            )
-            return result
-        except exc.ApiError as e:
-            raise exc.ImproperlyConfiguredError("X-Ray header config invalid") from e
+    _xray_header = None
 
     @classmethod
-    def _xray(self, request) -> str:
-        if self._xray_header in request.META:
+    @property
+    def xray_header(cls):
+        if cls._xray_header is None:
+            logger.info(f"<{cls.__name__}>. xray header requested")
             try:
-                xray = dpath_value(request.META, self._xray_header, str, transform=transform_uuid)
-                logger.debug(f"<{self.__class__.__name__}>. xray inherited from request header: {xray}")
+                cls._xray_header = get_settings_value(
+                    "LAMB_LOGGING_HEADER_XRAY", "LAMB_EVENT_LOGGING_HEADER_TRACKID", req_type=str, allow_none=False
+                )
+            except exc.ApiError as e:
+                logger.info(
+                    f"LAMB_LOGGING_HEADER_XRAY config is required for LambXRayMiddleware using. "
+                    f"Will use default HTTP_X_LAMB_XRAY value"
+                )
+                cls._xray_header = "HTTP_X_LAMB_XRAY"
+        return cls._xray_header
+
+    @classmethod
+    def _xray(cls, request) -> str:
+        if cls.xray_header in request.META:
+            try:
+                xray = dpath_value(request.META, cls.xray_header, str, transform=transform_uuid)
+                logger.debug(f"<{cls.__name__}>. xray inherited from request header: {xray}")
             except Exception as e:
-                logger.warning(f"<{self.__class__.__name__}>. xray extract failed: {e}")
+                logger.warning(f"<{cls.__name__}>. xray extract failed: {e}")
                 xray = uuid.uuid4()
         else:
             xray = uuid.uuid4()
