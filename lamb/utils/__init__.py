df723159edb828f545fc2c7996252d5e2855e055
Vladimir Konev <vk@escape-team.tech>
2025-01-31 12:32:48 +0300

    bytes humanize

--- lamb/utils/__init__.py
+++ lamb/utils/__init__.py
@@ -114,6 +114,7 @@ __all__ = [
     "tz_now",
     "TZ_MSK",
     "TZ_UTC",
+    "humanize_bytes",
 ]
 
 
@@ -724,6 +725,14 @@ def check_device_info_versions_above(
             continue
 
     return True
+
+
+def humanize_bytes(num, suffix="b"):
+    for unit in ("", "K", "M", "G", "T", "P", "E", "Z"):
+        if abs(num) < 1024.0:
+            return f"{num:3.1f}{unit}{suffix}"
+        num /= 1024.0
+    return f"{num:.1f}Y{suffix}"
 
 
 # time cached
