27b38b471908c2b1d2eb2da8483f83e9cfe43baf
Vladimir Konev <vk@escape-team.tech>
2026-09-01 20:19:39 +0300

    ref: middleware - abstract sync/async middlewares revised and doc update to respect less context switches

--- lamb/middleware/base.py
+++ lamb/middleware/base.py
@@ -3,6 +3,8 @@ import logging
 from asgiref.sync import iscoroutinefunction, markcoroutinefunction
 from django.http import HttpResponse
 
+from lamb.exc import ProgrammingError
+
 __all__ = ["LambMiddlewareMixin"]
 
 
@@ -13,34 +15,52 @@ class LambMiddlewareMixin:
     """
     Lamb base middleware aiming to omit context switches in before/after request processing.
 
-    - based on Django MiddlewareMixin but omit context switch penalties
-    - handles if applied on class using old-style middlewares
-    - use independent before/after logic
+    Main logic:
+        - checks if applied to old-style middlewares looking for methods `process_request`, `process_response` and `process_view` - would raise Exception in this case
+        - desired to support both sync and async modes to reduce context switches
+        - during django init step analyze is next layer is coroutine - in this case mark self as coroutine function
+        - during execution step analyze is act in async mode and in this case - switch to async call version
+        - provides independent sugar methods for children class to not implement custom __call__/__acall__ methods
+        - sugar methods `before_request`, `after_response` - should not use IO bound calls to not break context switches
+        - exception in underlying layers should be processed with classic `process_exception` method
+
+    NOTE: `process_exception` in case of async/sync IO bound operations required - could be complex task
+        - option 1: inspect hidden _lamb_error - like `LambExecutionTimeMiddleware`
+        - option 2: implement fully customized __call__/__acall__ method and may be even switch `process_response` in __init__ like
+
+        def __init__(self, get_response):
+            super().__init__(get_response)
+            if self.async_mode:
+                self.process_exception = self.process_exception_async
+            else:
+                self.process_exception = self.process_exception_sync
     """
 
     sync_capable = True
     async_capable = True
 
     def __init__(self, get_response):
+        # checks
         if get_response is None:
             raise ValueError("get_response must be provided.")
-        self.get_response = get_response
-        # If get_response is a coroutine function, turns us into async mode so
-        # a thread is not consumed during a whole request.
-        self.async_mode = iscoroutinefunction(self.get_response)
-        if self.async_mode:
-            # Mark the class as async-capable, but do the actual switch inside
-            # __call__ to avoid swapping out dunder methods.
-            markcoroutinefunction(self)
 
         for m in ["process_request", "process_response", "process_view"]:
             if hasattr(self, m):
-                raise ValueError(
+                raise ProgrammingError(
                     f"<{self.__class__.__name__}>: could not be used with old style middlewares. "
                     f"Use django.utils.deprecation.MiddlewareMixin instead"
                 )
 
-        super().__init__()
+        # construct and analyse next layer
+        self.get_response = get_response
+        # If next layer is async function
+        # - turn self into async mode - would be respected in call
+        # - mark self as async function - to force django respect it in context switch chain
+        self.async_mode = iscoroutinefunction(self.get_response)
+        if self.async_mode:
+            # Mark the class intself as async-capable, but do the actual switch inside
+            # __call__ to avoid swapping out dunder methods.
+            markcoroutinefunction(self)
 
     def __repr__(self):
         return "<{} get_response={}>".format(
@@ -53,25 +73,34 @@ class LambMiddlewareMixin:
         )
 
     def __call__(self, request) -> HttpResponse:
+        # Actually django in all cases invokes methods __call__
+        # - in sync mode - it is called as sync function `res = m(request)`
+        # - in async mode - it is called as async function `res = await m(request)`
+        # - so monkeypatch is easy - we understand our mode and in async mode returns async version
         # Exit out to async mode, if needed
-        logger.debug(f"<{self.__class__.__name__}>: Processing __call__")
         if self.async_mode:
             return self.__acall__(request)
+
+        # processing
+        logger.debug(f"<{self.__class__.__name__}>: Processing __call__")
         response = None
         if hasattr(self, "before_request"):
             response = self.before_request(request)
+
         response = response or self.get_response(request)
+
         if hasattr(self, "after_response"):
             response = self.after_response(request, response)
         return response
 
     async def __acall__(self, request) -> HttpResponse:
-        # Exit out to async mode, if needed
         logger.debug(f"<{self.__class__.__name__}>: Processing __acall__")
         response = None
         if hasattr(self, "before_request"):
             response = self.before_request(request)
+
         response = response or await self.get_response(request)
+
         if hasattr(self, "after_response"):
             response = self.after_response(request, response)
         return response
