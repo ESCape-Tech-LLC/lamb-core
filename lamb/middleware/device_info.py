70619d048bc13a23f87f3cfbc6d0a29afc9b9f55
Vladimir Konev <vk@escape-team.tech>
2026-09-01 20:23:50 +0300

    ref: middleware - device_info adapted to better sync/async compatibility and default locale attach removed(use cfactory version)

--- lamb/middleware/device_info.py
+++ lamb/middleware/device_info.py
@@ -1,10 +1,7 @@
 import logging
 
-from django.conf import settings
-
 from lamb.middleware.base import LambMiddlewareMixin
 from lamb.types.device_info_type import device_info_factory
-from lamb.types.locale_type import LambLocale
 
 logger = logging.getLogger(__name__)
 
@@ -15,13 +12,10 @@ __all__ = ["LambDeviceInfoMiddleware"]
 class LambDeviceInfoMiddleware(LambMiddlewareMixin):
     """Middleware parse and append device info and locale to request"""
 
+    sync_capable = True
+    async_capable = True
+
     def before_request(self, request):
         # attach device info
         request.lamb_device_info = device_info_factory(request)
-        logger.debug(f"<{self.__class__.__name__}>: Device info attached: {request.lamb_device_info}")
-
-        # attach device locale
-        if request.lamb_device_info.device_locale is not None:
-            request.lamb_locale = request.lamb_device_info.device_locale
-        else:
-            request.lamb_locale = LambLocale.parse(settings.LAMB_DEVICE_DEFAULT_LOCALE)
+        logger.debug(f"<{self.__class__.__name__}> attached: device_info={request.lamb_device_info}")
